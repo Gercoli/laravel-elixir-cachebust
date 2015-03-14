10dce2594c163498f3b151aa8a89bc534e4005bf
Garry Ercoli <Garry@GErcoli.com>
2015-03-14 13:42:33 -0500

    finished up hashFile() and now it needs testing

--- index.js
+++ index.js
@@ -48,14 +48,16 @@ function generateHash(str, length)
 }
 
 var hashFile = function(options) {
-    options = objectAssign({hash:'md4'},options || {});
+    options = objectAssign({method:'hash',length: 8},options || {});
 
     return through.obj(function(file,encoding,callback) {
+        // If we are not passed a valid object, abort.
         if(!file || file.isNull()) {
             callback(null,file);
             return;
         }
 
+        // We can't deal with a stream, abort.
         if(file.isStream()) {
             callback(
                 new gutil.PluginError('laravel-elixir-cachebust', 'Streaming not supported')
@@ -63,6 +65,41 @@ var hashFile = function(options) {
             return;
         }
 
+        // If this does not exist,
+        // then this is the first time that we have been passed this file.
+        file_mtime[file.path] = file_mtime[file.path] || 1;
 
+        var thisFile = {
+            current_mtime:  file.stat.mtime.getTime(),      // This files last modified date
+            saved_mtime:    file_mtime[file.path]["mtime"], // The last known modification date (according to our records)
+            saved_hash:     file_mtime[file.path]["hash"]   // The last known hash (according to our records)
+        };
+
+        // If the modified times don't match,
+        // or there is no existing hash, make one.
+        if( thisFile.current_mtime != thisFile.saved_mtime || !(thisFile.saved_hash) )
+        {
+            // The user can choose what method they want to use to cache-bust.
+            // Hashing, UUID, or file modified time.
+            if(options.method == 'hash') {
+                var strHash = generateHash(file.contents,options.length);
+            }
+            else if (options.method == 'uuid')
+            {
+                var strHash = uuid(options.length);
+            }
+            else
+            {
+                var strHash = "" + thisFile.current_mtime;
+            }
+
+            file_mtime[file.path] = {
+                mtime: thisFile.current_mtime,
+                hash: strHash
+            }
+        }
+
+        file.hash = file_mtime[file.path]['hash'];
+        callback(null,file);
     });
 }
